a6a54c23ffcd3471a60c7ac949b61c5fb526dd95
ibrahimy353 <85551204+ibrahimy353@users.noreply.github.com>
2022-05-27 10:40:55 +0300

    added multiples files

--- server.php
+++ server.php
@@ -7,7 +7,7 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'project');
+$db = mysqli_connect('localhost', 'root', '', 'login-page');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
